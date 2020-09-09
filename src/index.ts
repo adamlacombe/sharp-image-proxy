2005ef6329d4a42b75d425087ae8a1f854c3d484
Adam LaCombe <adam@edgeworkscreative.com>
2020-09-09 18:51:41 +0800

    catch FetchError

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,21 @@ const ALLOWED_HOST = process.env.ALLOWED_HOST;
 const app = express();
 
 const fetchImage = async (url: string) => {
-  const fetchReq = await fetch(url, { method: 'GET' });
+  try {
+    const fetchReq = await fetch(url, { method: 'GET' });
 
-  let format = url.split('.').pop();
-  if (fetchReq.headers.has('content-type') && fetchReq.headers.get('content-type').startsWith('image/')) {
-    format = fetchReq.headers.get('content-type').replace('image/', '');
-  }
+    let format = url.split('.').pop();
+    if (fetchReq.headers.has('content-type') && fetchReq.headers.get('content-type').startsWith('image/')) {
+      format = fetchReq.headers.get('content-type').replace('image/', '');
+    }
 
-  return {
-    status: fetchReq.status,
-    buffer: await fetchReq.buffer(),
-    format: format
+    return {
+      status: fetchReq.status,
+      buffer: await fetchReq.buffer(),
+      format: format
+    }
+  } catch (e) {
+    return { status: 500 }
   }
 };
 
@@ -46,7 +50,7 @@ app.get('/', async (req, res) => {
   const toFormat = (format === "avif") ? "heif" : format;
   const compression = (format === "avif") ? "av1" : undefined;
   const pipeline = sharp(image.buffer);
-  
+
   let width = (req.query.width) ? parseInt(req.query.width.toString()) : null;
   let height = (req.query.height) ? parseInt(req.query.height.toString()) : null;
   width = (width && (await pipeline.metadata()).width >= width) ? width : null;
